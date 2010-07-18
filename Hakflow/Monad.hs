efe30bbaca146560e866744401a050005c686e96
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2010-07-18 05:26:32 -0400

    Hakflow/Monad: HakState

--- Hakflow/Monad.hs
+++ Hakflow/Monad.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE
+  FlexibleInstances,
   GeneralizedNewtypeDeriving,
   MultiParamTypeClasses,
   PackageImports,
@@ -12,13 +13,24 @@ import Hakflow.Makeflow
 import Data.Maybe
 import System.Directory
 import "mtl" Control.Monad.RWS.Strict
-import qualified Data.Sequence as Seq
+import qualified Data.Vector as V
+import System.Random.Mersenne
 
 
-type Log = Seq.Seq String
+newtype Log = Log {unLog :: V.Vector String} deriving Show
+
+instance Monoid Log where
+    mempty = Log V.empty
+    mappend l1 l2 = Log $ unLog l1 V.++ unLog l2
+
 
 newtype Hak a = Hak {
-      runHak :: RWST Opts Log Command IO a
-    } deriving (Functor, Monad, MonadWriter Log, MonadReader Opts, MonadState Command, MonadIO)
+      runHak :: RWST Opts Log HakState IO a
+    } deriving (Functor, Monad, MonadWriter Log, MonadReader Opts, MonadState HakState, MonadIO)
 
 
+data HakState = HS {
+      counter :: !Integer
+    , randGen :: !MTGen
+    , workflow :: !Makeflow
+    }
